34ba4a7bab7dda20fd6e088329bd02d818fb3a37
Yadhunath2003 <yadhunath2003@gmail.com>
2024-04-13 20:49:27 -0500

    updated ryr.html

--- index.js
+++ index.js
@@ -1,88 +1,52 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var path = require("path");
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(bodyParser.json())
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
-    extended: true
-}));
+    extended:true
+}))
 
-mongoose.connect('mongodb://localhost:27017/login-page', {
+mongoose.connect('mongodb://localhost:27017/login-page',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 var db = mongoose.connection;
 
-db.on('error', () => console.log("Error in Connecting to Database"));
-db.once('open', () => console.log("Connected to Database"));
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
 
-// Define a schema for user data
-var userSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String
-});
-
-// Create a model from the schema
-var User = mongoose.model('User', userSchema);
-
-app.post("/sign_up", (req, res) => {
-    var { username, email, password } = req.body;
-
-    var newUser = new User({
-        username: username,
-        email: email,
-        password: password
-    });
-
-    newUser.save((err) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send("Error registering new user.");
-        } else {
-            console.log("Record Inserted Successfully");
-            res.redirect('signup_success.html');
-        }
-    });
-});
-
-app.post("/login", (req, res) => {
-    var { email, password } = req.body;
-
-    User.findOne({ email: email, password: password }, (err, user) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).send();
-        }
-        if (!user) {
-            return res.status(404).send('User not found');
-        }
-        return res.redirect('/RYR.html');
-    });
-});
-
+// Serve login.html when accessing the root URL
 app.get("/", (req, res) => {
-    res.set({
-        "Allow-access-Allow-Origin": '*'
+    res.sendFile(__dirname + '/public/login.html');
+});
+
+// Handle form submission from login.html
+app.post("/sign_up",(req,res)=>{
+    var username = req.body.username
+    var email = req.body.email;
+    var password = req.body.password;
+
+    var data = {
+        "username": username,
+        "email" : email,
+        "password" : password
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+        // Redirect to signup_success.html after successful form submission
+        res.sendFile(__dirname + '/public/signup_success.html');
     });
-    return res.redirect('login.html');
 });
 
 app.listen(3000, () => {
     console.log("Listening on PORT 3000");
-});
-
-// Serve 'RYR.html' from the 'public' directory
-app.get("/RYR.html", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/RYR.html'));
-});
-
-// Serve 'login.html' from the same directory as 'RYR.html'
-app.get("/login.html", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/login.html'));
 });
